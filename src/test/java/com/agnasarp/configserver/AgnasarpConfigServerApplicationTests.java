89460931c678005c0063ea3053802c0f200c6e9f
Agnasarp <info.agnasarp@gmail.com>
2021-05-24 22:32:01 +0530

    Initial commit

---
+++ src/test/java/com/agnasarp/configserver/AgnasarpConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnasarp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgnasarpConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
